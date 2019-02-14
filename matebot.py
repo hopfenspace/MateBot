4567f68190daebf9f02e662c00fc4e7f488528f4
Jakob Löw <jakob@m4gnus.de>
2019-02-14 16:50:44 +0100

    implement the communism command

--- matebot.py
+++ matebot.py
@@ -1,4 +1,5 @@
 import json, datetime, traceback
+import telegram
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 with open("config.json", "r") as fd:
@@ -8,6 +9,8 @@ with open("state.json", "r") as fd:
 	users = json.load(fd)
 
 logFd = open("transactions.log", "a")
+
+communisms = {}
 
 def saveState():
 	with open("state.json", "w") as fd:
@@ -101,7 +104,7 @@ def parseArgs(msg, argDef, usage=""):
 
 			result.append(user)
 		elif expected == ARG_REST:
-			result.append(split[i : ])
+			result.append(" ".join(split[i : ]))
 			break
 
 		offset = offset + len(arg) + 1
@@ -123,7 +126,7 @@ def balance(bot, update):
 def drink(bot, update):
 	user = getOrCreateUser(update.message.from_user)
 	createTransaction(user, -100, "drink")
-	update.message.reply_text("OK, enjoy your drink!")
+	update.message.reply_text("OK, enjoy your drink!", disable_notification=True)
 
 def send(bot, update):
 	args = parseArgs(update.message, [ARG_AMOUNT, ARG_USER], "\nUsage: /send <amount> <user>")
@@ -138,12 +141,102 @@ def send(bot, update):
 	update.message.reply_text("OK, you sent {}€ to {}"
 		.format(amount / float(100), receiver['name']))
 
+class Communism:
+	def __init__(self, creator, amount, reason):
+		self.creator = creator
+		self.amount = amount
+		self.reason = reason
+		self.members = [creator]
+		self.message = None
 
+		prefix = "communism " + str(creator['id'])
+		self.message_markup = telegram.InlineKeyboardMarkup([
+			[
+				telegram.InlineKeyboardButton("JOIN/LEAVE", callback_data=prefix + " join/leave"),
+			],
+			[
+				telegram.InlineKeyboardButton("OK", callback_data=prefix + " ok"),
+				telegram.InlineKeyboardButton("CANCEL", callback_data=prefix + " cancel"),
+			],
+		])
+
+	def amountEuro(self):
+		return self.amount / float(100)
+
+	def memberList(self):
+		names = []
+		for member in self.members:
+			names.append(member['name'])
+
+		return ", ".join(names)
+
+	def __str__(self):
+		return "Communism by {}\nAmount: {}\nReason: {}\nCommunists: {}\n" \
+			.format(self.creator['name'], self.amountEuro(), self.reason, self.memberList())
 
 def communism(bot, update):
-	pass #TODO
+	amount, reason = parseArgs(update.message, [ARG_AMOUNT, ARG_REST], "\nUsage: /communism <amount> <reason ...>")
 
+	sender = getOrCreateUser(update.message.from_user)
+	id = str(sender['id'])
 
+	if id in communisms:
+		update.message.reply_text("You already have a communism in progress")
+		return
+
+	communism = Communism(sender, amount, reason)
+	communism.message = update.message.reply_text(str(communism), reply_markup=communism.message_markup)
+	communisms[id] = communism
+
+def communismQuery(bot, update):
+	sender = getOrCreateUser(update.callback_query.from_user)
+	split = update.callback_query.data.split(" ")
+
+	if len(split) != 3:
+		print(split)
+		raise Exception("invalid callback query")
+	elif split[1] not in communisms:
+		print(split[1], communisms)
+		raise Exception("invalid ID")
+
+	communism = communisms[split[1]]
+	members = communism.members
+	isAdmin = sender == communism.creator
+
+	if split[2] == "join/leave":
+		if sender in members:
+			members.remove(sender)
+		else:
+			members.append(sender)
+
+		if len(members) == 0:
+			del communisms[split[1]]
+			communism.message.edit_text("Everyone left, the communism died")
+		else:
+			communism.message.edit_text(str(communism), reply_markup=communism.message_markup)
+	elif isAdmin and split[2] == "ok":
+		count = len(members)
+		amount = communism.amount // count
+
+		# if the amount can't be split equally eveyone pays 1 cent more
+		if communism.amount % count != 0:
+			amount = amount + 1
+
+		reason = "communism by " + communism.creator['nick']
+		for member in members:
+			createTransaction(member, -amount, reason)
+
+		createTransaction(communism.creator, communism.amount, reason)
+		del communisms[split[1]]
+
+		creator = communism.creator['name']
+		text = "Communism by {}\n{} paid {}\n{} received {}\nDescription: {}" \
+			.format(creator, communism.memberList(), amount / float(100), creator, communism.amountEuro(), communism.reason)
+		communism.message.edit_text(text)
+
+	elif isAdmin and split[2] == "cancel":
+		del communisms[split[1]]
+		communism.message.edit_text("Communism canceled")
 
 updater = Updater(config["bot-token"])
 
@@ -159,6 +252,9 @@ def tryWrap(func):
 updater.dispatcher.add_handler(CommandHandler("balance", tryWrap(balance)))
 updater.dispatcher.add_handler(CommandHandler("drink", tryWrap(drink)))
 updater.dispatcher.add_handler(CommandHandler("send", tryWrap(send)))
+updater.dispatcher.add_handler(CommandHandler("communism", tryWrap(communism)))
+
+updater.dispatcher.add_handler(CallbackQueryHandler(tryWrap(communismQuery), pattern="^communism"))
 
 updater.start_polling()
 updater.idle()
