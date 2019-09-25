981f5fa719b86d62d816e5a6da9c8c0414eac64f
Jakob Löw <jakob@m4gnus.de>
2019-09-25 15:20:35 +0200

    add a script for recalculating state from a start state and a transaction log

---
+++ recalculate-state.py
@@ -0,0 +1,30 @@
+import json, sys
+
+if len(sys.argv) != 4:
+	print("Usage: recalculate-state.py <start state> <transactions> <start timestamp>")
+	exit(0)
+
+with open(sys.argv[1], "r") as fd:
+	state = json.load(fd)
+
+start = float(sys.argv[3])
+with open(sys.argv[2], "r") as fd:
+	for line in fd.readlines():
+		entry = json.loads(line)
+
+		if entry["timestamp"] > start:
+
+			userId = str(entry["user"])
+
+			if userId not in state:
+				state[userId] = {
+					"id": int(userId),
+					"nick": "",
+					"name": "",
+					"balance": 0,
+				}
+
+			user = state[userId]
+			user["balance"] += entry["diff"]
+
+print(json.dumps(state))
