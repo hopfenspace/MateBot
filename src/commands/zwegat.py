f823444316bbf204fdd46d47894d2369f1fedfde
Jakob Löw <jakob@m4gnus.de>
2019-09-25 22:51:27 +0200

    fix indentation fails by @dennisdenk

--- src/commands/zwegat.py
+++ src/commands/zwegat.py
@@ -1,15 +1,14 @@
 from state import users
 
-
 def zwegat(bot, update):
-    total = 0
-    for id in users:
-        total -= users[id]["balance"]
+	total = 0
+	for id in users:
+		total += users[id]["balance"]
 
-    total = -1 * float(total) / 100
-    if total <= 0:
-        update.message.reply_text(
-            "Peter errechnet ein massives Vermögen von {}€".format(total * (-1)))
-    else:
-        update.message.reply_text(
-            "Peter errechnet Gesamtschulden von {}€".format(total))
+	total = float(total) / 100
+	if total <= 0:
+		update.message.reply_text(
+			"Peter errechnet ein massives Vermögen von {}€".format(-1 * total))
+	else:
+		update.message.reply_text(
+			"Peter errechnet Gesamtschulden von {}€".format(total))
