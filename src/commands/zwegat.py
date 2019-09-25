d8b39fa6bf64521a8593a490f6eef11ac5bd1311
Dennis Denk <dennisdenk@t-online.de>
2019-09-25 17:02:45 +0200

    Added Conditional reply on positive or negative balance

--- src/commands/zwegat.py
+++ src/commands/zwegat.py
@@ -1,9 +1,15 @@
 from state import users
 
-def zwegat(bot, update):
-	total = 0
-	for id in users:
-		total -= users[id]["balance"]
 
-	total = -1 * float(total) / 100
-	update.message.reply_text("Peter errechnet Gesamtschulden von {}€".format(total))
+def zwegat(bot, update):
+    total = 0
+    for id in users:
+        total -= users[id]["balance"]
+
+    total = -1 * float(total) / 100
+    if total <= 0:
+        update.message.reply_text(
+            "Peter errechnet ein massives Vermögen von {}€".format(total * (-1)))
+    else:
+        update.message.reply_text(
+            "Peter errechnet Gesamtschulden von {}€".format(total))
