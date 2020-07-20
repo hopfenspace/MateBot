d8fb9778b007f4ff2de537886fb037f7bfe46e88
myOmikron <accounts@omikron.pw>
2020-07-20 21:44:52 +0200

    parse_amount checks now if decimal place is only one digit

--- src/args.py
+++ src/args.py
@@ -13,6 +13,8 @@ def parse_amount(text, max_amount=config["max-amount"]):
     if match.group(3):
         if len(match.group(3)) > 2:
             return None, "too precise ({} only two decimals are supported)".format(match.group(0))
+        elif len(match.group(3)) == 1:
+            val += int(match.group(3) * 10)
         else:
             val += int(match.group(3))
 
