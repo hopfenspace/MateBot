82328dff7abb4fd150d9ce25ac3c3bb1d8f679a6
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-08-13 01:40:09 +0200

    Simlified object-dict conversion

--- src/state/io.py
+++ src/state/io.py
@@ -6,11 +6,7 @@ from .user import MateBotUser
 
 def load(fpath: str = "state.json") -> Dict[str, MateBotUser]:
     def dict_to_user(dct: Dict) -> MateBotUser:
-        return MateBotUser(None,
-                           dct["id"],
-                           dct["name"],
-                           dct["nick"],
-                           dct["balance"])
+        return MateBotUser(**dct)
 
     with open(fpath, "r") as fd:
         return dict((key, dict_to_user(dct)) for key, dct in json.load(fd).items())
@@ -18,12 +14,7 @@ def load(fpath: str = "state.json") -> Dict[str, MateBotUser]:
 
 def save(users: Dict[str, MateBotUser], fpath: str = "state.json"):
     def user_to_dict(user: MateBotUser) -> Dict:
-        return {
-            "id": user.id,
-            "name": user.name,
-            "nick": user.nick,
-            "balance": user.balance,
-        }
+        return user.__dict__
 
     with open(fpath, "w") as fd:
         json.dump(dict(((key, user_to_dict(user)) for key, user in users.items())), fd)
