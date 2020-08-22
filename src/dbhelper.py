a11322650ab4b014bc25287b3223a60556b0d975
Chris <chris@hopfen.space>
2020-08-22 22:07:58 +0200

    Added a database helper module to execute SQL queries

---
+++ src/dbhelper.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Database management helper
+"""
+
+import typing
+import pymysql as _pymysql
+
+from config import config as _config
+
+
+def execute(cmd: str) -> typing.Tuple[int, typing.Any]:
+    """
+    Connect to the database, execute a single query and return results
+
+    :param cmd: complete SQL query string without any placeholders
+    :type cmd: str
+    :return: tuple of number of the resulting query and the fetched data
+    :rtype: tuple
+    """
+
+    connection = _pymysql.connect(**_config["database"], cursorclass = _pymysql.cursors.DictCursor)
+    if connection.open:
+        connection.show_warnings()  # TODO
+        try:
+            with connection.cursor() as cursor:
+                state = cursor.execute(cmd)
+                connection.commit()
+                result = cursor.fetchall()
+        finally:
+            connection.close()
+    else:
+        raise _pymysql.err.OperationalError("No open connection")
+    return state, result
