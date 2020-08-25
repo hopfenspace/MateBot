357136e7e0af19d832853e826be6b751f3ee0b8b
Chris <chris@hopfen.space>
2020-08-26 00:20:17 +0200

    Cleaned up the __init__.py file, added __all__ to specify the public API

--- mate_bot/state/__init__.py
+++ mate_bot/state/__init__.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python3
 
-from typing import Union
-
 from .user import MateBotUser, CommunityUser
 from .transactions import Transaction, TransactionLog
 
 
-
-users = load("state.json")
-logFd = open("transactions.log", "a")
-
-
-
-
-def find_user_by_nick(nick: str) -> Union[MateBotUser, None]:
-    """
-    Find a user by his nickname.
-
-    :param nick: A user's nickname on Telegram
-    :type nick: str
-    :return: The user or ``None``
-    :rtype: MateBotUser or None
-    """
-
-    for user_id in users:
-        user = users[user_id]
-        if user.nick == nick:
-            return user
-    else:
-        return None
+__all__ = [
+    "MateBotUser",
+    "CommunityUser",
+    "Transaction",
+    "TransactionLog"
+]
