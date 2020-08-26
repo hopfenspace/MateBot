3de2c75f6896019881d013ac782a98a69c7c1e7f
Chris <chris@hopfen.space>
2020-08-26 17:23:48 +0200

    Renamed the helpers module, added its successor to the __init__.py

--- mate_bot/state/__init__.py
+++ mate_bot/state/__init__.py
@@ -2,11 +2,16 @@
 
 from .user import MateBotUser, CommunityUser
 from .transactions import Transaction, TransactionLog
+from .finders import find_user_by_name, find_user_by_username, find_names_by_pattern, find_usernames_by_pattern
 
 
 __all__ = [
     "MateBotUser",
     "CommunityUser",
     "Transaction",
-    "TransactionLog"
+    "TransactionLog",
+    "find_user_by_name",
+    "find_user_by_username",
+    "find_names_by_pattern",
+    "find_usernames_by_pattern"
 ]
