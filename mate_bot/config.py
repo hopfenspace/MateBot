a862c1f8f796e4f81ec5c4ea18cb72112190af17
Chris <chris@hopfen.space>
2020-08-31 16:46:53 +0200

    Extended the config module to allow a more flexible config handling

--- mate_bot/config.py
+++ mate_bot/config.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
-import json
+"""
+MateBot configuration provider
+"""
 
-with open("config.json", "r") as fd:
-    config = json.load(fd)
+import os as _os
+import json as _json
+
+
+if _os.path.exists("config.json"):
+    with open("config.json", "r") as f:
+        config = _json.load(f)
+
+elif _os.path.exists(_os.path.join("..", "config.json")):
+    with open(_os.path.join("..", "config.json")) as f:
+        config = _json.load(f)
+
+else:
+    raise ImportError("No configuration file found")
