a201dba00027460dc390e88135d41264ff6755f9
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-09-01 22:05:02 +0200

    Removed depricated args.py and added import in args/__init__.py

--- mate_bot/args/__init__.py
+++ mate_bot/args/__init__.py
@@ -6,3 +6,8 @@ this module handles the parsing of commands' arguments
 
 from .parser import NonExitingParser
 from .parser import ParsingError
+from .types import amount
+from .types import user
+from .actions import JoinAction
+from .pre_parser import pre_parse
+
