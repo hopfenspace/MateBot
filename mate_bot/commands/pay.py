05a25a3817594032ecfda87ee973f8bbfd464ac6
Chris <chris@hopfen.space>
2020-09-15 05:51:26 +0200

    Ensured that importing module pay does not break

--- mate_bot/commands/pay.py
+++ mate_bot/commands/pay.py
@@ -6,7 +6,6 @@ import argparse
 
 import telegram
 
-from mate_bot.commands.common_util import user_list_to_string
 from mate_bot.commands.base import BaseCommand
 from mate_bot.args.types import amount as amount_type
 from mate_bot.args.actions import JoinAction
@@ -37,11 +36,7 @@ class Pay:
         return self.amount / float(100)
 
     def __str__(self):
-        return f"Pay by {self.creator['name']}\n" \
-               f"Amount: {self.amount_euro() :.2f}€\n" \
-               f"Reason: {self.reason}\n" \
-               f"Approvers: {user_list_to_string(self.approved)}\n" \
-               f"Disapprovers: {user_list_to_string(self.disapproved)}\n"
+        raise NotImplementedError
 
 
 class PayCommand(BaseCommand):
@@ -52,18 +47,7 @@ class PayCommand(BaseCommand):
         self.parser.add_argument("reason", action=JoinAction, nargs="*")
 
     def run(self, args: argparse.Namespace, msg: telegram.Message) -> None:
-        """
-        sender = get_or_create_user(msg.from_user)
-        sender_id = str(sender['id'])
-
-        if sender_id in pays:
-            msg.reply_text("You already have a pay in progress")
-            return
-
-        user_pay = Pay(sender, args.amount, args.sreason)
-        user_pay.message = msg.reply_text(str(user_pay), reply_markup=user_pay.message_markup)
-        pays[sender_id] = user_pay
-        """
+        pass
 
 
 class PayQuery:
@@ -114,7 +98,7 @@ def pay_query(_, update):
     elif check_list(selected_pay.approved):
         del pays[sender_id]
         #create_transaction(selected_pay.creator, selected_pay.amount,
-        #                   "pay for {}, approved by {}".format(selected_pay.reason, user_list_to_string(selected_pay.approved)))
+        # "pay for {}, approved by {}".format(selected_pay.reason, user_list_to_string(selected_pay.approved)))
         selected_pay.message.edit_text("APPROVED\n" + str(selected_pay))
     elif changed:
         selected_pay.message.edit_text(str(selected_pay), reply_markup=selected_pay.message_markup)
