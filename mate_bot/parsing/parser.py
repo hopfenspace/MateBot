57c2e0ddbbb8c217789d503b8382ee756d957f4a
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-09-18 06:17:33 +0200

    Put _split_by_entities into _split by removing reversed order

--- mate_bot/parsing/parser.py
+++ mate_bot/parsing/parser.py
@@ -10,39 +10,29 @@ from mate_bot.parsing.string import EntityString
 class CommandParser(_AttributeHolder):
 
     def parse(self, msg: telegram.Message) -> Namespace:
-        arg_strings = self._split(msg)
+        arg_strings = list(self._split(msg))
         pass
 
     @staticmethod
-    def _split_by_entities(msg: telegram.Message) -> typing.Iterator[typing.Union[str, EntityString]]:
-        """
-        Split a telegram message by its entities.
-
-        All entities will become a separate piece and the raw text in between entities.
-        This iterator will give the split pieces in reverse order!
-
-        Danger!!!
-        Nested entities would probably break this.
-        """
-        text = msg.text
-
-        for entity in reversed(msg.entities):
-            # If there is normal text left after the entity
-            if entity.offset + entity.length < len(text):
-                yield from map(EntityString, filter(bool, reversed(text[entity.offset + entity.length:].split())))
-                text = text[:entity.offset + entity.length]
-
-            yield EntityString(text[entity.offset:], entity)
-            text = text[:entity.offset]
-
-        # Return left over text which might be before any entities
-        if text:
-            yield text
-
-    def _split(self, msg: telegram.Message) -> typing.List[EntityString]:
+    def _split(msg: telegram.Message) -> typing.Iterator[typing.Union[str, EntityString]]:
         """
         Split a telegram message into EntityStrings
 
         This functions splits by spaces while keeping entities intact.
+
+        Danger!!!
+        Nested entities would probably break this.
         """
-        return list(reversed(list(self._split_by_entities(msg))))
+        last_entity = 0
+
+        for entity in msg.entities:
+            # If there is normal text left before the next entity
+            if last_entity < entity.offset:
+                yield from map(EntityString, filter(bool, reversed(msg.text[last_entity:entity.offset].split())))
+
+            yield EntityString(msg.text[entity.offset:entity.offset + entity.length], entity)
+            last_entity = entity.offset + entity.length
+
+        # Return left over text which might be after the last entity
+        if msg.text[last_entity:]:
+            yield from map(EntityString, filter(bool, reversed(msg.text[last_entity:].split())))
