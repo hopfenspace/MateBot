dc31b9aa528e87227cb4c3d1fab1dca655ebd396
Chris <chris@hopfen.space>
2020-09-22 04:42:44 +0200

    Moved the _execute and _execute_no_commit functions to the BackendHelper class

--- mate_bot/state/dbhelper.py
+++ mate_bot/state/dbhelper.py
@@ -75,80 +75,6 @@ DATABASE_SCHEMA = {
 }
 
 
-def _execute_no_commit(
-        query: str,
-        arguments: typing.Union[tuple, list, dict, None] = None,
-        connection: typing.Optional[pymysql.connections.Connection] = None
-) -> EXECUTE_NO_COMMIT_TYPE:
-    """
-    Connect to the database, execute a single query and return results and the database connection
-
-    Use this function in case you need more than one query to fulfill your needs.
-    All your queries will be cached on the server side as long as you don't call
-    the .commit() method on the returned Connection object to save the changes
-    you introduced during your previous queries. Important: When the program exits
-    or the connection is closed without calling .commit(), the introduced changes
-    are lost! So, be careful and enclose this call in a try-finally-block:
-
-        try:
-            rows, result, connection = execute_no_commit(...)
-            ...
-            connection.commit()
-        finally:
-            if connection:
-                connection.close()
-
-    :param query: SQL query string that might contain placeholders
-    :type query: str
-    :param arguments: optional collection of arguments that should be passed into the query
-    :type arguments: tuple, list, dict or None
-    :param connection: optional connection to the database (it will be opened implicitly if None)
-    :type connection: typing.Optional[pymysql.connections.Connection]
-    :return: number of affected rows, the fetched data and the open database connection
-    :rtype: tuple
-    :raises TypeError: when the connection is neither None nor a valid Connection
-    :raises pymysql.err.OperationalError: when the database connection is closed
-    """
-
-    if connection is None:
-        connection = pymysql.connect(**_config["database"], cursorclass=pymysql.cursors.DictCursor)
-    elif not isinstance(connection, pymysql.connections.Connection):
-        raise TypeError("Invalid connection type")
-
-    if connection.open:
-        with connection.cursor() as cursor:
-            rows = cursor.execute(query, arguments)
-            result = list(cursor.fetchall())
-    else:
-        raise pymysql.err.OperationalError("No open connection")
-    return rows, result, connection
-
-
-def _execute(
-        query: str,
-        arguments: typing.Union[tuple, list, dict, None] = None
-) -> EXECUTE_TYPE:
-    """
-    Connect to the database, execute and commit a single query and return results
-
-    :param query: SQL query string that might contain placeholders
-    :type query: str
-    :param arguments: optional collection of arguments that should be passed into the query
-    :type arguments: tuple, list, dict or None
-    :return: number of affected rows and the fetched data
-    :rtype: tuple
-    """
-
-    connection = None
-    try:
-        rows, result, connection = _execute_no_commit(query, arguments)
-        connection.commit()
-    finally:
-        if connection:
-            connection.close()
-    return rows, result
-
-
 class BackendHelper:
     """
     Helper class providing easy methods to read and write values in the database
@@ -159,6 +85,84 @@ class BackendHelper:
     actual database query language. Any high level implementation
     may subclass this class in order to declare its area of usage.
     """
+
+    @staticmethod
+    def _execute_no_commit(
+            query: str,
+            arguments: typing.Union[tuple, list, dict, None] = None,
+            connection: typing.Optional[pymysql.connections.Connection] = None
+    ) -> EXECUTE_NO_COMMIT_TYPE:
+        """
+        Connect to the database, execute a single query and return results and the connection
+
+        Use this function in case you need more than one query to fulfill your needs.
+        All your queries will be cached on the server side as long as you don't call
+        the .commit() method on the returned Connection object to save the changes
+        you introduced during your previous queries. Important: When the program exits
+        or the connection is closed without calling .commit(), the introduced changes
+        are lost! So, be careful and enclose this call in a try-finally-block:
+
+            try:
+                rows, result, connection = execute_no_commit(...)
+                ...
+                connection.commit()
+            finally:
+                if connection:
+                    connection.close()
+
+        :param query: SQL query string that might contain placeholders
+        :type query: str
+        :param arguments: optional collection of arguments that should be passed into the query
+        :type arguments: tuple, list, dict or None
+        :param connection: optional connection to the database (opened implicitly if None)
+        :type connection: typing.Optional[pymysql.connections.Connection]
+        :return: number of affected rows, the fetched data and the open database connection
+        :rtype: tuple
+        :raises TypeError: when the connection is neither None nor a valid Connection
+        :raises pymysql.err.OperationalError: when the database connection is closed
+        """
+
+        if connection is None:
+            connection = pymysql.connect(
+                **_config["database"],
+                cursorclass=pymysql.cursors.DictCursor
+            )
+
+        elif not isinstance(connection, pymysql.connections.Connection):
+            raise TypeError("Invalid connection type")
+
+        if connection.open:
+            with connection.cursor() as cursor:
+                rows = cursor.execute(query, arguments)
+                result = list(cursor.fetchall())
+        else:
+            raise pymysql.err.OperationalError("No open connection")
+        return rows, result, connection
+
+    @staticmethod
+    def _execute(
+            query: str,
+            arguments: typing.Union[tuple, list, dict, None] = None
+    ) -> EXECUTE_TYPE:
+        """
+        Connect to the database, execute and commit a single query and return results
+
+        :param query: SQL query string that might contain placeholders
+        :type query: str
+        :param arguments: optional collection of arguments that should be passed into the query
+        :type arguments: tuple, list, dict or None
+        :return: number of affected rows and the fetched data
+        :rtype: tuple
+        """
+
+        connection = None
+        try:
+            rows, result, connection = BackendHelper._execute_no_commit(query, arguments)
+            connection.commit()
+        finally:
+            if connection:
+                connection.close()
+        return rows, result
 
     @staticmethod
     def set_value(
@@ -209,7 +213,7 @@ class BackendHelper:
         if column not in DATABASE_SCHEMA[table]:
             raise ValueError(f"Unknown column '{column}' in table '{table}'")
 
-        return _execute(
+        return BackendHelper._execute(
             f"UPDATE {table} SET {column}=%s WHERE id=%s",
             (value, identifier)
         )
