adff145e4b82d31564026ed0e408acfff3663e07
Chris <chris@hopfen.space>
2020-09-26 05:54:52 +0200

    Added the first version of the command-line interface

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+import argparse
 
 from telegram.ext import (
     Updater, CommandHandler,
@@ -22,6 +23,65 @@ from mate_bot.commands.communism import (
 from mate_bot.commands.send import SendCallbackQuery
 
 
+class MateBotRunner:
+    """
+    MateBot application executor
+    """
+
+    def __init__(self, args: argparse.Namespace) -> None:
+        pass
+
+    @staticmethod
+    def setup() -> argparse.ArgumentParser:
+        """
+        Setup the ArgumentParser to provide the command-line interface
+
+        :return: ArgumentParser
+        :rtype: argparse.ArgumentParser
+        """
+
+        parser = argparse.ArgumentParser(
+            description = "MateBot maintaining command-line interface"
+        )
+
+        parser.add_argument(
+            "-v", "--verbose",
+            help = "print out verbose information",
+            dest = "verbose",
+            action = "store_true"
+        )
+
+        subcommands = parser.add_subparsers(
+            title = "available subcommands",
+            dest = "command",
+            required = True
+        )
+
+        run = subcommands.add_parser(
+            "run",
+            help = "run the MateBot program"
+        )
+
+        install = subcommands.add_parser(
+            "install",
+            help = "install the MateBot database and systemd service files"
+        )
+
+        database.add_argument(
+            "-s", "--show",
+            help = "show all data stored in the specified table",
+            dest = "data",
+            metavar = "table"
+        )
+
+        extract = subcommands.add_parser(
+            "extract",
+            help = "extract the raw data from the MateBot database"
+        )
+
+        return parser
+
+
 COMMANDS = {
     Filters.all: COMMANDS.commands_as_dict
 }
@@ -39,6 +99,9 @@ HANDLERS = {
 
 
 if __name__ == "__main__":
+    arguments = MateBotRunner.setup().parse_args()
+    exit(MateBotRunner(arguments).start())
+
     log.setup()
     logger = logging.getLogger()
 
