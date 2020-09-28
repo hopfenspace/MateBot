ae610d46ead6ea19d1e11446b4716bbf42226c2b
Chris <chris@hopfen.space>
2020-09-28 03:03:00 +0200

    Added the handling of externals and showing current debtors for /vouch

--- mate_bot/commands/vouch.py
+++ mate_bot/commands/vouch.py
@@ -7,6 +7,7 @@ import telegram
 from mate_bot.commands.base import BaseCommand
 from mate_bot.parsing import types
 from mate_bot.parsing.util import Namespace
+from mate_bot.state.user import MateBotUser
 
 
 class VouchCommand(BaseCommand):
@@ -31,5 +32,35 @@ class VouchCommand(BaseCommand):
         :type update: telegram.Update
         :return: None
         """
+
+        owner = MateBotUser(update.effective_message.from_user)
+        if owner.external:
+            update.effective_message.reply_text("You can't perform this command.")
+            return
+
+        if args.user is None:
+            debtors = ", ".join(map(
+                lambda u: f"{u.name} ({u.username})" if u.username else u.name,
+                map(
+                    lambda i: MateBotUser(i),
+                    owner.debtors
+                )
+            ))
+
+            if len(debtors) == 0:
+                update.effective_message.reply_text(
+                    "You don't vouch for any external user at the moment. "
+                    "To change this, use `/vouch <username>`.",
+                    parse_mode = "Markdown"
+                )
+
+            else:
+                update.effective_message.reply_text(
+                    "You currently vouch for the following "
+                    f"user{'s' if len(debtors) != 1 else ''}: {debtors}",
+                    parse_mode="Markdown"
+                )
+
+            return
 
         update.effective_message.reply_text("This feature is not implemented yet.")
