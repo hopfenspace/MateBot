6c0693a912e07f7c7a4f692a7d7413c26d084d56
Crsi <47722349+CrsiX@users.noreply.github.com>
2020-10-01 23:14:16 +0200

    Added the possibility to consume more than one good at once (#8)
    * Added the possibility to consume more than one good at once
    
    * Renamed get_amount_helper to CamelCase
    
    * Improved the reply messages when the amount is negative, zero or too high
    
    * Changed the emojis for /water, slightly improved coding style
    
    * Fixed the broken default value

--- src/commands/consume.py
+++ src/commands/consume.py
@@ -1,29 +1,63 @@
 from state import getOrCreateUser, createTransaction
 import random
+from args import parseArgs, ARG_INT
+
+
+def getAmountHelper(msg, name) -> int:
+	args = parseArgs(msg, [ARG_INT], [1], "/{} [amount]".format(name))
+	if isinstance(args[0], int):
+		if args[0] == 0:
+			msg.reply_text("You can't consume zero {}s".format(name))
+			return 0
+		elif args[0] < 0:
+			msg.reply_text("You can't consume a negative number of {}s".format(name))
+			return 0
+		elif args[0] > 10:
+			msg.reply_text("You can't consume more than 10 {}s at once!".format(name))
+			return 0
+		return args[0]
+	else:
+		msg.reply_text("Unknown parsing error")
+		return 0
+
 
 def drink(bot, update):
-	user = getOrCreateUser(update.message.from_user)
-	createTransaction(user, -100, "drink")
-	update.message.reply_text("OK, enjoy your 🍹!", disable_notification=True)
+	num = getAmountHelper(update.message, "drink")
+	if num > 0:
+		user = getOrCreateUser(update.message.from_user)
+		createTransaction(user, -100 * num, "drink x{}".format(num))
+		update.message.reply_text("OK, enjoy your {}!".format(num * '🍹'), disable_notification=True)
+
 
 hydrationMessages = [
-	"OK, enjoy your 🍼!",
-	"HYDRATION! 💦",
-	"Hydrier dich!",
-	"Hydrieren sie sich bitte!",
-	"Der Bahnbabo sagt: Hydriert euch! 💪"
+	("OK, enjoy your {}!", "🍼"),
+	("HYDRATION! {}", "🍶"),
+	("Hydrier dich! {}", "💦"),
+	("Hydrieren sie sich bitte! {}", "💧"),
+	("Der Bahnbabo sagt: Hydriert euch! {}", "💪")
 ]
+
+
 def water(bot, update):
-	user = getOrCreateUser(update.message.from_user)
-	createTransaction(user, -50, "water")
-	update.message.reply_text(random.choice(hydrationMessages), disable_notification=True)
+	num = getAmountHelper(update.message, "water")
+	if num > 0:
+		user = getOrCreateUser(update.message.from_user)
+		createTransaction(user, -50 * num, "water x{}".format(num))
+		answer = random.choice(hydrationMessages)
+		update.message.reply_text(answer[0].format(num * answer[1]), disable_notification=True)
+
 
 def pizza(bot, update):
-	user = getOrCreateUser(update.message.from_user)
-	createTransaction(user, -200, "pizza")
-	update.message.reply_text("Buon appetito! 🍕", disable_notification=True)
+	num = getAmountHelper(update.message, "pizza")
+	if num > 0:
+		user = getOrCreateUser(update.message.from_user)
+		createTransaction(user, -200 * num, "pizza x{}".format(num))
+		update.message.reply_text("Buon appetito! {}".format(num * '🍕'), disable_notification=True)
+
 
 def ice(bot, update):
-	user = getOrCreateUser(update.message.from_user)
-	createTransaction(user, -50, "ice")
-	update.message.reply_text("Have a sweet one! 🚅", disable_notification=True)
+	num = getAmountHelper(update.message, "ice")
+	if num > 0:
+		user = getOrCreateUser(update.message.from_user)
+		createTransaction(user, -50 * num, "ice x{}".format(num))
+		update.message.reply_text("Have a sweet one! {}".format(num * '🚅'), disable_notification=True)
