c37bf1baa8d3ef5fa6137e8de65fc7d4190bfb10
Chris <chris@hopfen.space>
2020-10-07 04:36:54 +0200

    Rewrote the __init__.py file for the commands package

--- mate_bot/commands/__init__.py
+++ mate_bot/commands/__init__.py
@@ -1,34 +1,49 @@
 """
-MateBot collection of commands
+MateBot collection of command executors
 """
 
 from mate_bot.config import config
-from mate_bot.commands.registry import COMMANDS
 from mate_bot.commands.balance import BalanceCommand
-from mate_bot.commands.communism import CommunismCommand
-from mate_bot.commands.data import DataCommand
-from mate_bot.commands.help import HelpCommand
-from mate_bot.commands.history import HistoryCommand
-from mate_bot.commands.pay import PayCommand
-from mate_bot.commands.send import SendCommand
-from mate_bot.commands.start import StartCommand
 from mate_bot.commands.blame import BlameCommand
-from mate_bot.commands.vouch import VouchCommand
+from mate_bot.commands.communism import CommunismCommand, CommunismCallbackQuery
+from mate_bot.commands.data import DataCommand
+from mate_bot.commands.forward import ForwardInlineQuery, ForwardInlineResult
+from mate_bot.commands.help import HelpCommand, HelpInlineQuery
+from mate_bot.commands.history import HistoryCommand
+from mate_bot.commands.pay import PayCommand, PayCallbackQuery
+from mate_bot.commands.send import SendCommand, SendCallbackQuery
+from mate_bot.commands.start import StartCommand
+from mate_bot.commands.vouch import VouchCommand, VouchCallbackQuery
 from mate_bot.commands.zwegat import ZwegatCommand
 from mate_bot.commands.consume import ConsumeCommand
 
 
-# Register all commands
-COMMANDS.add(BalanceCommand())
-COMMANDS.add(CommunismCommand())
-COMMANDS.add(DataCommand())
-COMMANDS.add(HelpCommand())
-COMMANDS.add(HistoryCommand())
-COMMANDS.add(PayCommand())
-COMMANDS.add(SendCommand())
-COMMANDS.add(StartCommand())
-COMMANDS.add(BlameCommand())
-COMMANDS.add(VouchCommand())
-COMMANDS.add(ZwegatCommand())
+# In order to register all executors in the registry, we just
+# have to create an object of their corresponding class. The
+# constructors of the base classes care about adding the
+# specific executor object to the correct registry pool.
+
+BalanceCommand()
+BlameCommand()
+CommunismCommand()
+DataCommand()
+HelpCommand()
+HistoryCommand()
+PayCommand()
+SendCommand()
+StartCommand()
+VouchCommand()
+ZwegatCommand()
+
 for consumable in config["consumables"]:
-    COMMANDS.add(ConsumeCommand(**consumable))
+    ConsumeCommand(**consumable)
+
+CommunismCallbackQuery()
+PayCallbackQuery()
+SendCallbackQuery()
+VouchCallbackQuery()
+
+ForwardInlineQuery(r"^\d+(\s?\S?)*")
+HelpInlineQuery(r"")
+
+ForwardInlineResult(r"^forward-\d+-\d+-\d+")
