bd2f6b3b92e4211e1cd3dc231e87aa4f7e76a156
Chris <chris@hopfen.space>
2020-10-07 05:06:31 +0200

    Fixed the error that the base classes depend on the registry

--- mate_bot/registry.py
+++ mate_bot/registry.py
@@ -2,17 +2,10 @@
 MateBot registry of available executors
 """
 
-from typing import Dict as _Dict
-
-from mate_bot.commands.base import (
-    BaseCommand as _BaseCommand,
-    BaseCallbackQuery as _BaseCallbackQuery,
-    BaseInlineQuery as _BaseInlineQuery,
-    BaseInlineResult as _BaseInlineResult
-)
 
 
-commands: _Dict[str, _BaseCommand] = {}
-callback_queries: _Dict[str, _BaseCallbackQuery] = {}
-inline_queries: _Dict[str, _BaseInlineQuery] = {}
-inline_results: _Dict[str, _BaseInlineResult] = {}
+
+commands: dict = {}
+callback_queries: dict = {}
+inline_queries: dict = {}
+inline_results: dict = {}
