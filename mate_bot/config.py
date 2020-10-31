1614bfd5d34b7b0269c36761be1f3642c57847d8
Chris <chris@hopfen.space>
2020-10-31 02:40:29 +0100

    Added a read-only class for later storage of the config dictionary

--- mate_bot/config.py
+++ mate_bot/config.py
@@ -4,6 +4,24 @@ MateBot configuration provider
 
 import os as _os
 import json as _json
+import logging as _logging
+
+
+class ReadOnlyDict(dict):
+    """
+    Read-only variant of a dictionary that also supports access via attributes
+    """
+
+    def __setitem__(self, key, value):
+        _logging.getLogger("config").warning(
+            f"Ignoring write access to the config ({key}:{value})"
+        )
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __getattr__(self, item):
+        return self[item]
 
 
 if _os.path.exists("config.json"):
