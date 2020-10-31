b62d5fb6e80645dbd9714a61a79c23fb2de17f4f
Chris <chris@hopfen.space>
2020-10-31 02:59:34 +0100

    Added a class Configuration, removed the old plain loading
    Note that the old way to import the configuration is still valid.
    You can still use `from mate_bot.config import config` to get it.

--- mate_bot/config.py
+++ mate_bot/config.py
@@ -2,8 +2,9 @@
 MateBot configuration provider
 """
 
-import os as _os
-import json as _json
+import os
+import json
+import typing
 import logging as _logging
 
 
@@ -24,13 +25,44 @@ class ReadOnlyDict(dict):
         return self[item]
 
 
-if _os.path.exists("config.json"):
-    with open("config.json", "r") as f:
-        config = _json.load(f)
+class Configuration:
+    """
+    Configuration storage via its property attribute :attr:`data`
+    """
 
-elif _os.path.exists(_os.path.join("..", "config.json")):
-    with open(_os.path.join("..", "config.json")) as f:
-        config = _json.load(f)
+    _content: str
+    _data: typing.Optional[ReadOnlyDict]
+    _path: str
 
+    def __init__(self, config_file: str):
+        self._path = config_file
+        with open(self._path, "r") as f:
+            self._content = f.read()
+        self._data = None
+
+    def validate(self) -> bool:
+        return True  # TODO
+
+    @property
+    def data(self) -> typing.Optional[ReadOnlyDict]:
+        """
+        Get the full config data as read-only dictionary
+        """
+
+        if self._data is None:
+            if not self.validate():
+                raise ValueError("Config validation was not successful")
+            self._data = ReadOnlyDict(**json.loads(self._content), _path=self._path)
+        return self._data
+
+
+for check in [
+    "config.json",
+    os.path.join("..", "config.json")
+]:
+    if os.path.exists(check):
+        configuration = Configuration(check)
+        config = configuration.data
+        break
 else:
     raise ImportError("No configuration file found")
