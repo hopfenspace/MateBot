3a64879486174edc07f7a8d9c226a0987115ed72
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-19 20:07:19 +0100

    Added both use cases to help command

--- mate_bot/commands/help.py
+++ mate_bot/commands/help.py
@@ -2,9 +2,7 @@
 MateBot command executor classes for /help
 """
 
-import typing
 import logging
-import datetime
 
 from nio import AsyncClient, RoomMessageText, MatrixRoom
 
@@ -12,7 +10,6 @@ from mate_bot import registry
 from mate_bot.commands.base import BaseCommand
 from mate_bot.parsing.types import command as command_type
 from mate_bot.parsing.util import Namespace
-from mate_bot.state.user import MateBotUser
 
 
 logger = logging.getLogger("commands")
@@ -38,8 +35,10 @@ class HelpCommand(BaseCommand):
         """
         :param args: parsed namespace containing the arguments
         :type args: argparse.Namespace
-        :param update: incoming Telegram update
-        :type update: telegram.Update
+        :param room: room the message came in
+        :type room: nio.MatrixRoom
+        :param event: incoming message event
+        :type event: nio.RoomMessageText
         :return: None
         """
 
@@ -48,9 +47,23 @@ class HelpCommand(BaseCommand):
             msg = f"*Usages:*\n{usages}\n\n*Description:*\n{args.command.description}"
 
         else:
-            #user = MateBotUser(update.effective_message.from_user)
-            #msg = self.get_help_usage(registry.commands, self.usage, user)
-            msg = "NotImplemented"
+            command_list = "\n".join(map(lambda c: f" - `{c}`", sorted(registry.commands.keys())))
+            msg = f"{self.usage}\n\nList of commands:\n\n{command_list}"
+
+            '''
+            user = MateBotUser(event.sender)
+            if user and isinstance(user, MateBotUser) and user.external:
+                msg += "\n\nYou are an external user. Some commands may be restricted."
+
+                if user.creditor is None:
+                    msg += (
+                        "\nYou don't have any creditor. Your possible interactions "
+                        "with the bot are very limited for security purposes. You "
+                        "can ask some internal user to act as your voucher. To "
+                        "do this, the internal user needs to execute `/vouch "
+                        "<your username>`. Afterwards, you may use this bot."
+                    )
+            '''
 
         await self.client.room_send(
             room.room_id,
@@ -59,39 +72,3 @@ class HelpCommand(BaseCommand):
             ignore_unverified_devices=True
         )
         #update.effective_message.reply_markdown(msg)
-
-    @staticmethod
-    def get_help_usage(
-            commands: dict,
-            usage: str,
-            user: typing.Optional[MateBotUser] = None
-    ) -> str:
-        """
-        Retrieve the help message from the help command without arguments
-
-        :param commands: dictionary of registered commands, see :mod:`mate_bot.registry`
-        :type commands: dict
-        :param usage: usage string of the help command
-        :type usage: str
-        :param user: optional MateBotUser object who issued the help command
-        :type user: typing.Optional[MateBotUser]
-        :return: fully formatted help message when invoking the help command without arguments
-        :rtype: str
-        """
-
-        command_list = "\n".join(map(lambda c: f" - `{c}`", sorted(commands.keys())))
-        msg = f"{usage}\n\nList of commands:\n\n{command_list}"
-
-        if user and isinstance(user, MateBotUser) and user.external:
-            msg += "\n\nYou are an external user. Some commands may be restricted."
-
-            if user.creditor is None:
-                msg += (
-                    "\nYou don't have any creditor. Your possible interactions "
-                    "with the bot are very limited for security purposes. You "
-                    "can ask some internal user to act as your voucher. To "
-                    "do this, the internal user needs to execute `/vouch "
-                    "<your username>`. Afterwards, you may use this bot."
-                )
-
-        return msg
