f5be99b35718617d83ad0c343ec8e71802b7a3c1
myOmikron <accounts@omikron.dev>
2020-11-19 18:31:24 +0100

    Added install.sh - without database

---
+++ install.sh
@@ -0,0 +1,9 @@
+pacman -S libolm python python-pip
+useradd -m -s /bin/bash mate_bot
+cp -r . /home/mate_bot
+cp mate_bot.service /etc/systemd/system/multi-user.target.wants/
+systemctl daemon-reload
+systemctl enable mate_bot.service
+chown -R mate_bot:mate_bot /home/mate_bot
+python -m venv /home/mate_bot/venv
+/home/mate_bot/venv/bin/python -m pip install -r requirements.txt
