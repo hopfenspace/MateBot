33a68faf3bb21e4fd52d70408ec771b1485bf70f
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-19 18:52:54 +0100

    Removed state from main

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ from hopfenmatrix.run import run
 from hopfenmatrix.callbacks import apply_filter, auto_join, allowed_rooms
 
 from mate_bot.config import config
-from mate_bot.state.dbhelper import BackendHelper
+#from mate_bot.state.dbhelper import BackendHelper
 from mate_bot.commands.base import BaseCommand
 
 
 async def main():
+    '''
     BackendHelper.db_config = config["database"]
     BackendHelper.query_logger = logging.getLogger("database")
     BackendHelper.get_value("users")
+    '''
 
     client = new_async_client(config)
     client.add_event_callback(apply_filter(auto_join(client), allowed_rooms(config.room)), InviteEvent)
