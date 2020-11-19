8413404478f03c9ad607c007d60a208a52eac345
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-19 17:48:15 +0100

    Made matebot's config a subclass of hopfenmatrix' Config

--- mate_bot/config.py
+++ mate_bot/config.py
@@ -1,68 +1,136 @@
 """
 MateBot configuration provider
 """
+import logging.config
 
-import os
-import json
-import typing
-import logging as _logging
+from hopfenmatrix.config import Config, Namespace
 
 
-class ReadOnlyDict(dict):
-    """
-    Read-only variant of a dictionary that also supports access via attributes
-    """
+class MateBotConfig(Config):
 
-    def __setitem__(self, key, value):
-        _logging.getLogger("config").warning(
-            f"Ignoring write access to the config ({key}:{value})"
-        )
+    def __init__(self):
+        super(MateBotConfig, self).__init__()
 
-    def __setattr__(self, key, value):
-        self[key] = value
+        self.general = Namespace()
+        self.general.max_amount = 10000
+        self.general.max_consume = 10
+        self.general.db_localtime = False
 
-    def __getattr__(self, item):
-        return self[item]
+        self.chats = Namespace()
+        self.chats.internal = 0
+        self.chats.transactions = []
+        self.chats.notification = []
+        self.chats.stacktrace = []
+        self.chats.debugging = []
+
+        self.community = Namespace()
+        self.community.payment_consent = 2
+        self.community.payment_denial = 2
+        self.community.multiple_externals = True
+
+        self.database = Namespace()
+        self.database.host = "localhost"
+        self.database.port = 3306
+        self.database.db = "mate_db"
+        self.database.user = "matebot_user"
+        self.database.password = "password"
+        self.database.charset = "utf8mb4"
+
+        self.testing = Namespace()
+        self.testing.db = "mate_db_test"
+
+        self.logging = Namespace()
+        self.logging.version = 1
+        self.logging.disable_existing_loggers = True
+        self.logging.incremental = False
+
+        self.logging.root = Namespace()
+        self.logging.root.level = "DEBUG"
+        self.logging.root.handlers = ["console", "file"]
+
+        self.logging.formatters = Namespace()
+        self.logging.handlers = Namespace()
+        self.logging.formatters.console = {
+            "style": "{",
+            "class": "logging.Formatter",
+            "format": "{asctime}: MateBot {process}: [{levelname}] {name}: {message}",
+            "datefmt": "%d.%m.%Y %H:%M"
+        }
+        self.logging.formatters.file = {
+            "style": "{",
+            "class": "logging.Formatter",
+            "format": "matebot {process}: [{levelname}] {name}: {message}",
+            "datefmt": "%d.%m.%Y %H:%M"
+        }
+        self.logging.handlers.console = {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "console",
+            "stream": "ext://sys.stdout"
+        }
+        self.logging.handlers.file = {
+            "level": "DEBUG",
+            "class": "logging.handlers.WatchedFileHandler",
+            "formatter": "file",
+            "filename": "matebot.log",
+            "encoding": "UTF-8"
+        }
+
+        self.logging.loggers = Namespace()
+        self.logging.loggers.collectives = {}
+        self.logging.loggers.commands = {}
+        self.logging.loggers.config = {}
+        self.logging.loggers.database = {}
+        self.logging.loggers.error = {}
+        self.logging.loggers.state = {}
+
+        self.consumables = [
+            {
+                "name": "drink",
+                "description": "",
+                "price": 100,
+                "messages": [
+                    "Okay, enjoy your "
+                ],
+                "symbol": "\uD83E\uDDC9"
+            },
+            {
+                "name": "water",
+                "description": "",
+                "price": 50,
+                "messages": [
+                    "HYDRATION! ",
+                    "Hydrier dich mit ",
+                    "Hydrieren sie sich bitte mit ",
+                    "Der Bahnbabo sagt: Hydriert euch mit ",
+                    "Okay, enjoy your "
+                ],
+                "symbol": "\uD83D\uDCA7"
+            },
+            {
+                "name": "pizza",
+                "description": "",
+                "price": 200,
+                "messages": [
+                    "Okay, enjoy your ",
+                    "Buon appetito! "
+                ],
+                "symbol": "\uD83C\uDF55"
+            },
+            {
+                "name": "ice",
+                "description": "",
+                "price": 50,
+                "messages": [
+                    "Okay, enjoy your ",
+                    "Hmmh, yummy... "
+                ],
+                "symbol": "\uD83C\uDF68"
+            }
+        ]
+
+    def setup_logging(self):
+        logging.config.dictConfig(self.logging)
 
 
-class Configuration:
-    """
-    Configuration storage via its property attribute :attr:`data`
-    """
-
-    _content: str
-    _data: typing.Optional[ReadOnlyDict]
-    _path: str
-
-    def __init__(self, config_file: str):
-        self._path = config_file
-        with open(self._path, "r") as f:
-            self._content = f.read()
-        self._data = None
-
-    def validate(self) -> bool:
-        return True  # TODO
-
-    @property
-    def data(self) -> typing.Optional[ReadOnlyDict]:
-        """
-        Get the full config data as read-only dictionary
-        """
-
-        if self._data is None:
-            if not self.validate():
-                raise ValueError("Config validation was not successful")
-            self._data = ReadOnlyDict(**json.loads(self._content), _path=self._path)
-        return self._data
-
-
-for check in [
-    "config.json",
-    os.path.join("..", "config.json")
-]:
-    if os.path.exists(check):
-        configuration = Configuration(check)
-        config = configuration.data
-        break
-else:
-    raise ImportError("No configuration file found")
+config = MateBotConfig.from_json("config.json")
