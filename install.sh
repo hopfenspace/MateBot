a95811687bbfe2ab43990a52b62b50ad4b21b9c8
myOmikron <accounts@omikron.dev>
2020-11-19 18:38:32 +0100

    Fixed correct directory for systemd

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 pacman -S libolm python python-pip
 useradd -m -s /bin/bash mate_bot
 cp -r . /home/mate_bot
-cp mate_bot.service /etc/systemd/system/multi-user.target.wants/
+cp mate_bot.service /usr/lib/systemd/system/
+ln -s /usr/lib/systemd/system/mate_bot.service /etc/systemd/system/multi-user.target.wants/
 systemctl daemon-reload
 systemctl enable mate_bot.service
 chown -R mate_bot:mate_bot /home/mate_bot
