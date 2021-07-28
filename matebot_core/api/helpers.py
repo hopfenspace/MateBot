09adc3932ac98a48415ee7716d7d667034de18d2
Chris <chris@hopfen.space>
2021-07-28 23:49:04 +0200

    Added yet another helper method to create new models in the DB

--- matebot_core/api/helpers.py
+++ matebot_core/api/helpers.py
@@ -2,10 +2,13 @@
 Generic helper library for the core REST API
 """
 
-from typing import List, Type
+import logging
+from typing import List, Optional, Type
 
 import pydantic
+import sqlalchemy.exc
 
+from .base import APIException
 from .dependency import LocalRequestData
 from ..persistence import models
 
@@ -31,3 +34,49 @@ def get_all_of_model(
     local.entity.model_name = model.__name__
     local.entity.compare(all_schemas)
     return local.attach_headers(all_schemas, **kwargs)
+
+
+def create_new_of_model(
+        model: models.Base,
+        local: LocalRequestData,
+        logger: Optional[logging.Logger] = None,
+        **kwargs
+) -> pydantic.BaseModel:
+    """
+    Create a new model's entry in the database
+
+    This method will also take care of handling any conditional request headers
+    and setting the correct ``ETag`` header (besides the others) in the response.
+
+    :param model: instance of a new SQLAlchemy model without any associated session
+    :param local: contextual local data
+    :param logger: optional logger that should be used for INFO and ERROR messages
+    :param kwargs: dictionary of additional headers for the response
+    :return: resulting object (as its schema's instance)
+    :raises APIException: when the database operation went wrong (to report the problem)
+    """
+
+    local.entity.model_name = type(model).__name__
+    local.entity.compare(None)
+
+    if logger is not None:
+        logger.info(f"Adding new model {model!r}...")
+
+    try:
+        local.session.add(model)
+        local.session.commit()
+    except sqlalchemy.exc.DBAPIError as exc:
+        local.session.rollback()
+
+        if logger is not None:
+            details = exc.statement.replace("\n", "")
+            logger.error(f"{type(exc).__name__}: {', '.join(exc.args)} @ {details!r}")
+
+        raise APIException(
+            status_code=400,
+            detail=f"Problem arguments: {', '.join(exc.args)!r}",
+            repeat=False,
+            message=f"Database error: {type(exc).__name__!r}"
+        )
+
+    return local.attach_headers(model.schema, **kwargs)
