eb31ee9c004a4ca22b085f093aad2b03cff41dca
Chris <chris@hopfen.space>
2021-07-28 03:47:43 +0200

    Added a model name attribute instead of the header fields for ETags

--- matebot_core/api/etag.py
+++ matebot_core/api/etag.py
@@ -28,26 +28,34 @@ class ETag:
     Helper class providing methods to create and compare ETags and related headers
     """
 
+    request: Request
+    model_name: Optional[str]
+
     def __init__(self, request: Request):
         self.request = request
-        self.match = request.headers.get("If-Match")
-        self.none_match = request.headers.get("If-None-Match")
+        self.model_name = None
 
-    def add_header(self, response: Response, model: base.ModelType):
+        if request.headers.get("If-None-Match"):
+            logger.warning("'If-None-Match' header not fully supported yet.")
+            logger.debug(f"Field value{request.headers.get('If-None-Match')!r}")
+
+    def add_header(self, response: Response, model: base.ModelType) -> bool:
         """
         Add the ETag header field of the model to the response
 
         :param response: Response object of the handled request
         :param model: generated model of the completely finished request
+        :return: whether the ETag header has been set on the response
         """
 
-        tag = self.make_etag(model)
+        tag = self.make_etag(model, self.model_name)
         if tag is not None:
             if not tag.startswith('"'):
                 tag = '"' + tag
             if not tag.endswith('"'):
                 tag += '"'
             response.headers.append("ETag", tag)
+        return tag is not None
 
     def compare(self, current_model: Optional[base.ModelType] = None) -> bool:
         """
@@ -66,7 +74,7 @@ class ETag:
         """
 
         verified = False
-        model_tag = self.make_etag(current_model)
+        model_tag = self.make_etag(current_model, self.model_name)
 
         precondition_failed = base.PreconditionFailed(
             self.request.url.path,
@@ -99,16 +107,18 @@ class ETag:
             return True
 
         # Step 1: evaluate the `If-Match` header precondition
-        if self.match is not None and self.match != "":
-            if not evaluate_match(self.match):
+        match = self.request.headers.get("If-Match")
+        if match is not None and match != "":
+            if not evaluate_match(match):
                 raise precondition_failed
             if self.request.method == "GET":
                 raise base.NotModified(self.request.url.path)
             verified = True
 
         # Step 2: evaluate the `If-None-Match` header precondition
-        if self.none_match is not None and self.none_match != "":
-            if not evaluate_none_match(self.none_match):
+        none_match = self.request.headers.get("If-None-Match")
+        if none_match is not None and none_match != "":
+            if not evaluate_none_match(none_match):
                 if self.request.method == "GET":
                     raise base.NotModified(self.request.url.path)
                 raise precondition_failed
@@ -121,13 +131,14 @@ class ETag:
         return True
 
     @staticmethod
-    def make_etag(obj: Any) -> Optional[str]:
+    def make_etag(obj: Any, name: Optional[str] = None) -> Optional[str]:
         """
-        Create a static, weak and unambiguous ETag value based on a given object
+        Create a static and unambiguous ETag value based on a given object
 
         The method might return None in case the generation of the ETag failed.
 
         :param obj: any object that can be JSON-serialized
+        :param name: optional string describing the object type (e.g. class name)
         :return: optional ETag value as a string
         """
 
@@ -155,5 +166,5 @@ class ETag:
 
         cls = type(obj).__name__
         dump = json.dumps(representation, allow_nan=False)
-        content = cls + dump + base.runtime_key
+        content = cls + dump + base.runtime_key + (name or "")
         return str(uuid.UUID(hashlib.md5(content.encode("UTF-8")).hexdigest()))
