3b2a28de54266efe3ecaac082a8b14678ddda745
Chris <chris@hopfen.space>
2021-08-06 04:26:35 +0200

    Added get_suite to tests.__init__ and added a __main__.py for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,13 @@
 """
 MateBot core unit tests
 """
+
+import unittest
+from . import persistence, schemas
+
+
+def get_suite() -> unittest.TestSuite:
+    suite = unittest.TestSuite()
+    suite.addTests(persistence.get_suite())
+    suite.addTests(schemas.get_suite())
+    return suite
---
+++ tests/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Run MateBot core unittests
+"""
+
+
+if __name__ == '__main__':
+    from .persistence import *
+    from .schemas import *
+    unittest.main()
