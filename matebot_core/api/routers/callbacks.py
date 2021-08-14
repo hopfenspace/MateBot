1b10fb19a796588903871284997ae9a3352e87bf
Chris <chris@hopfen.space>
2021-08-15 01:32:41 +0200

    Added some extra content to the endpoint descriptions

--- matebot_core/api/routers/callbacks.py
+++ matebot_core/api/routers/callbacks.py
@@ -18,6 +18,14 @@ callback_router = APIRouter(
 def trigger_cache_refresh():
     """
     Trigger a refresh of the local object caches.
+
+    This endpoint must be present on the external API, since it will always
+    be called when some internal states of the MateBot core have changed.
+    It will be called before the other, more specific endpoints below.
+
+    Implementation detail: The response of the external API implementation
+    will be logged in case of an error, but will be ignored in general.
+    The external API does not need to return any meaningful response.
     """
 
 
@@ -25,13 +33,31 @@ def trigger_cache_refresh():
 def announce_created_model():
     """
     Announce a created object of the ``model`` with the ``id``.
+
+    This endpoint may be seen as optional, since the external API is not
+    strictly required to implement this endpoint (but a meaningful error,
+    like 404, should be returned, of course). The endpoint will be called
+    after the response to the ``/trigger`` request has arrived.
+
+    Implementation detail: The response of the external API implementation
+    will be logged in case of an error, but will be ignored in general.
+    The external API does not need to return any meaningful response.
     """
 
 
 @callback_router.get("/update/{model}/{id}")
 def announce_updated_model():
     """
-    Announce a updated object of the ``model`` with the ``id``.
+    Announce an updated object of the ``model`` with the ``id``.
+
+    This endpoint may be seen as optional, since the external API is not
+    strictly required to implement this endpoint (but a meaningful error,
+    like 404, should be returned, of course). The endpoint will be called
+    after the response to the ``/trigger`` request has arrived.
+
+    Implementation detail: The response of the external API implementation
+    will be logged in case of an error, but will be ignored in general.
+    The external API does not need to return any meaningful response.
     """
 
 
@@ -39,6 +65,15 @@ def announce_updated_model():
 def announce_deleted_model():
     """
     Announce a deleted object of the ``model`` with the ``id``.
+
+    This endpoint may be seen as optional, since the external API is not
+    strictly required to implement this endpoint (but a meaningful error,
+    like 404, should be returned, of course). The endpoint will be called
+    after the response to the ``/trigger`` request has arrived.
+
+    Implementation detail: The response of the external API implementation
+    will be logged in case of an error, but will be ignored in general.
+    The external API does not need to return any meaningful response.
     """
 
 
