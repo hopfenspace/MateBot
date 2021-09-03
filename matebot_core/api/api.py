4ed825111ab27c3d75f7978c7edd4e98f63fc102
Chris <chris@hopfen.space>
2021-09-04 00:02:49 +0200

    Added a new FastAPI factory for the sub-API instances

--- matebot_core/api/api.py
+++ matebot_core/api/api.py
@@ -9,24 +9,100 @@ for more information about the breaking changes between versions.
 
 import os
 import logging.config
-from typing import Optional
+from typing import Any, Callable, Dict, Optional, Type, Union
 
 import fastapi.applications
 from fastapi.exceptions import RequestValidationError, StarletteHTTPException
 
 try:
     from fastapi.staticfiles import StaticFiles
-    static_docs = True
 except ImportError:
     StaticFiles = None
-    static_docs = False
 
-from . import base, versioning
+from . import api_v1, base, versioning
 from .routers import all_routers
 from .. import schemas, __version__
 from ..persistence import database
 from ..settings import Settings
 from .. import __file__ as _package_init_path
+
+
+DEFAULT_EXCEPTION_HANDLERS = {
+    base.APIException: base.APIException.handle,
+    RequestValidationError: base.APIException.handle,
+    StarletteHTTPException: base.APIException.handle,
+    Exception: base.APIException.handle
+}
+
+LICENSE_INFO = {
+    "name": "GNU General Public License v3",
+    "url": "https://www.gnu.org/licenses/gpl-3.0.html"
+}
+
+
+def _make_app(
+        title: str,
+        version: str,
+        description: str,
+        license_info: Optional[Dict[str, str]] = None,
+        static_directory: Optional[str] = None,
+        exception_handlers: Optional[Dict[Exception, Callable]] = None,
+        root_redirect: bool = True,
+        responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None,
+        api_class: Optional[Type[fastapi.FastAPI]] = None,
+        **kwargs
+) -> fastapi.FastAPI:
+    if api_class is None:
+        api_class = fastapi.FastAPI
+    app = api_class(
+        title=title,
+        version=version,
+        description=description,
+        license_info=license_info or LICENSE_INFO,
+        docs_url=None if static_directory else "/docs",
+        redoc_url=None if static_directory else "/redoc",
+        responses=responses or {422: {"model": schemas.APIError}},
+        **kwargs
+    )
+
+    handlers = exception_handlers or DEFAULT_EXCEPTION_HANDLERS
+    for exc in handlers:
+        app.add_exception_handler(exc, handlers[exc])
+
+    if root_redirect:
+        @app.get("/", include_in_schema=False)
+        async def redirect_root():
+            return fastapi.responses.RedirectResponse("/docs")
+
+    if static_directory:
+        app.mount("/static", StaticFiles(directory=static_directory), name="static")
+
+        @app.get("/redoc", include_in_schema=False)
+        async def get_redoc():
+            return fastapi.applications.get_redoc_html(
+                openapi_url=app.openapi_url,
+                title=app.title + " - ReDoc",
+                redoc_js_url="/static/redoc.standalone.js",
+                redoc_favicon_url="/static/img/favicon.ico",
+                with_google_fonts=False
+            )
+
+        @app.get("/docs", include_in_schema=False)
+        async def get_swagger_ui():
+            return fastapi.applications.get_swagger_ui_html(
+                openapi_url=app.openapi_url,
+                title=app.title + " - Swagger UI",
+                oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+                swagger_js_url="/static/swagger-ui-bundle.js",
+                swagger_css_url="/static/swagger-ui.css",
+                swagger_favicon_url="/static/img/favicon.ico"
+            )
+
+        @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+        async def get_swagger_ui_redirect():
+            return fastapi.applications.get_swagger_ui_oauth2_redirect_html()
+
+    return app
 
 
 def create_app(
@@ -60,24 +136,6 @@ def create_app(
     if configure_database:
         database.init(settings.database.connection, settings.database.echo)
 
-    app = versioning.VersionedFastAPI(
-        title="MateBot core REST API",
-        version=__version__,
-        docs_url=None if static_docs and configure_static_docs else "/docs",
-        redoc_url=None if static_docs and configure_static_docs else "/redoc",
-        description=__doc__,
-        responses={422: {"model": schemas.APIError}}
-    )
-
-    app.add_exception_handler(base.APIException, base.APIException.handle)
-    app.add_exception_handler(RequestValidationError, base.APIException.handle)
-    app.add_exception_handler(StarletteHTTPException, base.APIException.handle)
-    app.add_exception_handler(Exception, base.APIException.handle)
-
-    @app.get("/", include_in_schema=False)
-    async def get_root():
-        return fastapi.responses.RedirectResponse("/docs")
-
     static_dirs = [
         static_directory for static_directory in [
             os.path.join(os.path.abspath("."), "static"),
@@ -88,39 +146,33 @@ def create_app(
     if len(static_dirs) > 1:
         logger.warning("More than one static directory found! Though unexpected, it may be fine.")
 
-    if static_docs and configure_static_docs and StaticFiles and len(static_dirs) > 0:
-        app.mount("/static", StaticFiles(directory=static_dirs[0]), name="static")
-
-        @app.get("/redoc", include_in_schema=False)
-        async def get_redoc():
-            return fastapi.applications.get_redoc_html(
-                openapi_url=app.openapi_url,
-                title=app.title + " - ReDoc",
-                redoc_js_url="/static/redoc.standalone.js",
-                redoc_favicon_url="/static/img/favicon.ico",
-                with_google_fonts=False
-            )
-
-        @app.get("/docs", include_in_schema=False)
-        async def get_swagger_ui():
-            return fastapi.applications.get_swagger_ui_html(
-                openapi_url=app.openapi_url,
-                title=app.title + " - Swagger UI",
-                oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-                swagger_js_url="/static/swagger-ui-bundle.js",
-                swagger_css_url="/static/swagger-ui.css",
-                swagger_favicon_url="/static/img/favicon.ico"
-            )
-
-        @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
-        async def get_swagger_ui_redirect():
-            return fastapi.applications.get_swagger_ui_oauth2_redirect_html()
-
-    elif static_docs and configure_static_docs and StaticFiles:
+    static_directory = None
+    if configure_static_docs and StaticFiles and len(static_dirs) == 0:
         logger.warning("Configuring static files failed since some resources were not found.")
-    elif configure_static_docs and not static_docs:
+    elif configure_static_docs and not StaticFiles:
         logger.error("Configuring static files was not possible due to unmet dependencies!")
+    else:
+        static_directory = static_dirs[0]
 
+    app = _make_app(
+        title="MateBot core REST API",
+        version=__version__,
+        description=__doc__,
+        apis={
+            1: _make_app(
+                title="MateBot core REST API v1",
+                version="1.0",
+                description=api_v1.__doc__
+            ),
+        },
+        logger=logger,
+        license_info=LICENSE_INFO,
+        static_directory=static_directory,
+        responses={422: {"model": schemas.APIError}},
+        api_class=versioning.VersionedFastAPI
+    )
+
+    assert isinstance(app, versioning.VersionedFastAPI), "'VersionedFastAPI' instance required"
     for router in all_routers:
         app.add_router(router)
 
