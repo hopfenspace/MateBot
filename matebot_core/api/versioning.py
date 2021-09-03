d80bde59f327bcb8247f743c869b2c3b049cf11a
Chris <chris@hopfen.space>
2021-09-03 03:19:21 +0200

    Added decorators for the path operation functions to annotate the supported API version

--- matebot_core/api/versioning.py
+++ matebot_core/api/versioning.py
@@ -2,7 +2,37 @@
 MateBot API library to provide multiple versions of the API endpoints
 """
 
+from typing import Callable, Dict, List, Optional
+
 from fastapi import FastAPI, APIRouter
+
+
+VERSION_ANNOTATION_NAME = "_api_versions"
+MINIMAL_VERSION_ANNOTATION_NAME = "_minimal_api_version"
+
+
+def version(version_annotation: int) -> Callable[[Callable], Callable]:
+    def decorator(func: Callable) -> Callable:
+        annotated_versions = getattr(func, VERSION_ANNOTATION_NAME, [])
+        annotated_versions.append(version_annotation)
+        setattr(func, VERSION_ANNOTATION_NAME, annotated_versions)
+        return func
+    return decorator
+
+
+def versions(version_annotations: List[int]) -> Callable[[Callable], Callable]:
+    def decorator(func: Callable) -> Callable:
+        assert not hasattr(func, VERSION_ANNOTATION_NAME), "'versions' can't be used twice"
+        setattr(func, VERSION_ANNOTATION_NAME, version_annotations)
+        return func
+    return decorator
+
+
+def min_version(version_annotation: int) -> Callable[[Callable], Callable]:
+    def decorator(func: Callable) -> Callable:
+        setattr(func, MINIMAL_VERSION_ANNOTATION_NAME, version_annotation)
+        return func
+    return decorator
 
 
 class VersionedFastAPI(FastAPI):
