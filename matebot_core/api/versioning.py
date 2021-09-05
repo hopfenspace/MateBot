76fd45d969d46350edd99b45adc593aab5213e3c
Chris <chris@hopfen.space>
2021-09-05 02:58:41 +0200

    Added docstrings to all public objects in the versioning module

--- matebot_core/api/versioning.py
+++ matebot_core/api/versioning.py
@@ -20,6 +20,16 @@ def versions(
         minimal: Optional[int] = None,
         maximal: Optional[int] = None
 ) -> Callable[[Callable], Callable]:
+    """
+    Decorate a path operation function with the version(s) of the API that should support it
+
+    :param annotations: any number of explicit API versions that should include the decorated
+        path operation (which can't be below or above the minimal and maximal values respectively)
+    :param minimal: minimal version of APIs that should include the decorated path operation
+    :param maximal: maximal version of APIs that should include the decorated path operation
+    :return: decorator to use on a path operation function
+    """
+
     if not all(map(lambda x: isinstance(x, int), annotations)):
         raise TypeError(f"Not all annotations are integers: {annotations!r}")
     if minimal:
@@ -49,6 +59,45 @@ def versions(
 
 
 class VersionedFastAPI(fastapi.FastAPI):
+    """
+    Specialized FastAPI adding support for multiple versioned sub-APIs
+
+    Use this class as in-place replacement for the previous FastAPI
+    class. The constructor requires some new keyword arguments
+    as well as the ``apis`` parameter which should be the definition
+    which APIs should finally be loaded. Only API versions mentioned
+    in this dictionary will be enabled later on during ``finish``.
+
+    To add a new router to the API, use ``add_router`` instead
+    of ``include_router``, since the latter would just add it and
+    ignore the whole versioning part. After adding all the routers,
+    call the ``finish`` method once to create and include the sub
+    API definitions correctly with all path operations that were
+    annotated to be included by the specific API version. It would
+    be possible to omit certain API versions in the dictionary,
+    those versions would obviously just be missing in the result.
+
+    .. code-block::
+
+        app = VersionedFastAPI(
+            title="API",
+            apis={
+                1: FastAPI(
+                    title="API v1",
+                    version="1.0"
+                ),
+                2: FastAPI(
+                    title="API v2",
+                    version="2.0"
+                )
+            }
+        )
+        app.add_router(...)
+        app.add_router(...)
+        ...
+        app.finish()
+    """
+
     def __init__(
             self,
             apis: Dict[int, fastapi.FastAPI],
@@ -69,6 +118,12 @@ class VersionedFastAPI(fastapi.FastAPI):
         self._finished = False
 
     def finish(self, versions_endpoint: bool = True):
+        """
+        Complete the registration of new routers and build the relevant API routes once
+
+        :param versions_endpoint: switch to enable the special ``/versions`` endpoint
+        """
+
         if self._finished:
             return
 
@@ -100,6 +155,15 @@ class VersionedFastAPI(fastapi.FastAPI):
         self._finished = True
 
     def add_router(self, router: fastapi.APIRouter, **kwargs):
+        """
+        Add the routes of the router to the set of sub-APIs which fulfill their requirements
+
+        :param router: APIRouter carrying all routes that should be filtered and added
+        :param kwargs: optional keyword arguments for the ``include_router`` method of the
+            ``FastAPI`` instances which were supplied via the constructor's ``apis`` argument
+        :raises TypeError: when there are problems with the annotated values of the endpoints
+        """
+
         if self._finished:
             raise RuntimeError("Can't add new routers after the API has been finally built")
 
