717271bc17a3eca521f55f58aee925d5981a83c9
Chris <chris@hopfen.space>
2021-12-13 05:54:51 +0100

    Added some first checks to the single transaction test case

--- tests/misc.py
+++ tests/misc.py
@@ -3,7 +3,11 @@ MateBot unit tests for helpers and other miscellaneous features
 """
 
 import unittest as _unittest
+import logging
 from typing import Type
+
+from matebot_core.persistence import models
+from matebot_core.misc import transactions
 
 from . import utils
 
@@ -25,26 +29,35 @@ class TransactionTests(utils.BasePersistenceTests):
         self.session.add_all(self.get_sample_users())
         self.session.commit()
 
-    def test_restrictions_single(self):
+    def test_single_transactions(self):
+        user1 = self.session.query(models.User).get(1)
+        user4 = self.session.query(models.User).get(4)
+        user1_balance = user1.balance
+        user4_balance = user4.balance
+
+        # Negative or zero amount is forbidden
+        for v in [0, -1, -2, -52, 0.3, 0.9, -1e7]:
+            with self.assertRaises(ValueError):
+                transactions.create_transaction(user1, user4, v, "", self.session, logging.getLogger())
+        self.assertEqual(user1_balance, user1.balance)
+        self.assertEqual(user4_balance, user4.balance)
+
+        total = 0
+        for i, v in enumerate([3, 41, 51, 9, 3]):
+            t = transactions.create_transaction(user4, user1, v, "", self.session, logging.getLogger())
+            total += v
+            self.assertEqual(t.id, i+1)
+            self.assertEqual(t.amount, v)
+            self.assertEqual(t.multi_transaction, None)
+            self.assertEqual(user1.balance, user1_balance + total)
+            self.assertEqual(user4.balance, user4_balance - total)
+            self.assertEqual(i+1, len(self.session.query(models.Transaction).all()))
+
+    def test_one_to_many_transactions(self):
         pass
 
-    def test_creation_single(self):
+    def test_many_to_one_transactions(self):
         pass
 
-    def test_restrictions_one_to_many(self):
-        pass
-
-    def test_creation_one_to_many(self):
-        pass
-
-    def test_restrictions_many_to_one(self):
-        pass
-
-    def test_creation_many_to_one(self):
-        pass
-
-    def test_restrictions_matrix(self):
-        pass
-
-    def test_creation_matrix(self):
+    def test_matrix_transactions(self):
         pass
