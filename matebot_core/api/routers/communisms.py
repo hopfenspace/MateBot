d87398e7d3d3995e19cf1c05d9beccea64659098
Chris <chris@hopfen.space>
2022-02-20 04:30:44 +0100

    Rewrote the communism endpoints to create a new one, created the endpoint to handle participants

--- matebot_core/api/routers/communisms.py
+++ matebot_core/api/routers/communisms.py
@@ -3,7 +3,7 @@ MateBot router module for /communisms requests
 """
 
 import logging
-from typing import List, Union
+from typing import List
 
 import pydantic
 from fastapi import APIRouter, Depends
@@ -21,20 +21,32 @@ logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/communisms", tags=["Communisms"])
 
 
-async def _check_participants(communism: Union[schemas.Communism, schemas.CommunismCreation], local: LocalRequestData):
-    participants = {
-        p.user_id: await helpers.return_one(p.user_id, models.User, local.session)
-        for p in communism.participants
-    }
-    if [
-        k for k in participants
-        if not participants[k].active or (participants[k].external and participants[k].voucher is None)
-    ]:
-        raise BadRequest("Disabled users or externals without voucher can't participate in communisms.")
-    if len(communism.participants) != len(participants):
-        raise Conflict(
-            message="At least one user was mentioned more than once in the communism member list",
-            detail=str(communism.participants)
+def _compress_participants(participants: List[schemas.CommunismUserBinding]) -> List[schemas.CommunismUserBinding]:
+    compressed = {}
+    for binding in participants:
+        if binding.user_id not in compressed:
+            compressed[binding.user_id] = 0
+        compressed[binding.user_id] += binding.quantity
+    return [schemas.CommunismUserBinding(user_id=k, quantity=v) for k, v in compressed.items()]
+
+
+async def _check_participants(participants: List[schemas.CommunismUserBinding], local: LocalRequestData):
+    def restrict_community_user(special_flag: bool):
+        if special_flag:
+            raise Conflict("The community user can't participate in communisms.")
+
+    invalid_participant_entries = [
+        (k, v) for k, v in {
+            p.user_id: await helpers.return_one(p.user_id, models.User, local.session)
+            for p in participants
+        }.items()
+        if not v.active or (v.external and v.voucher_user is None) or restrict_community_user(v.special)
+    ]
+    if invalid_participant_entries:
+        raise BadRequest(
+            f"Disabled users or externals without voucher can't participate in communisms. "
+            f"{len(invalid_participant_entries)} proposed participants are disabled or external.",
+            str(invalid_participant_entries)
         )
 
 
@@ -55,7 +67,7 @@ async def get_all_communisms(local: LocalRequestData = Depends(LocalRequestData)
     "",
     status_code=201,
     response_model=schemas.Communism,
-    responses={400: {"model": schemas.APIError}, 404: {"model": schemas.APIError}}
+    responses={k: {"model": schemas.APIError} for k in (400, 404, 409)}
 )
 @versioning.versions(minimal=1)
 async def create_new_communism(
@@ -63,39 +75,38 @@ async def create_new_communism(
         local: LocalRequestData = Depends(LocalRequestData)
 ):
     """
-    Create a new communism based on the specified data.
+    Create a new communism based on the specified data
 
-    A 400 error will be returned if any participant was mentioned
-    more than one time. A 404 error will be returned if the user ID
-    of the `creator` or any mentioned participant is unknown.
+    A 400 error will be returned if the creator or any participant is an external
+    user without voucher, or if the creator or any participant is disabled.
+    A 404 error will be returned if the user ID of the
+    creator user or any mentioned participant is unknown.
+    A 409 error will be returned if the community user is part of it.
     """
 
     creator = await helpers.return_one(communism.creator_id, models.User, local.session)
+    if creator.special:
+        raise Conflict("The community user can't open communisms.")
     if not creator.active:
-        raise BadRequest("A disabled user can't create communisms.")
-    if creator.external and creator.voucher_id is None:
+        raise BadRequest("Your user account was deleted. Therefore, you can't create communisms.")
+    if creator.external and creator.voucher_user is None:
         raise BadRequest("You can't create communisms without having a voucher user.")
 
-    await _check_participants(communism, local)
+    participants = _compress_participants(communism.participants)
+    await _check_participants(participants, local)
 
     model = models.Communism(
         amount=communism.amount,
         description=communism.description,
         creator=creator,
         active=communism.active,
-        participants=[]
+        participants=[
+            models.CommunismUsers(user_id=p.user_id, quantity=p.quantity)
+            for p in participants
+        ]
     )
 
-    async def hook(*_):
-        local.session.add_all([
-            models.CommunismUsers(communism_id=model.id, user_id=p.user_id, quantity=p.quantity)
-            for p in communism.participants
-        ])
-        local.session.commit()
-
-    return await helpers.create_new_of_model(model, local, logger, hook_func=hook)
-
-
+    return await helpers.create_new_of_model(model, local, logger)
 
 
 @router.get(
@@ -222,4 +233,48 @@ async def close_open_communism(
 
     model.multi_transaction = m
     logger.debug(f"Closing communism {model} (created multi transaction {m} with {len(ts)} parts)")
+    return await helpers.update_model(model, local, logger, helpers.ReturnType.SCHEMA)
+
+
+@router.post(
+    "/setParticipants/{communism_id}",
+    response_model=schemas.Communism,
+    responses={k: {"model": schemas.APIError} for k in (400, 404)}
+)
+@versioning.versions(1)
+async def set_participants_of_open_communism(
+        communism_id: pydantic.NonNegativeInt,
+        participants: List[schemas.CommunismUserBinding],
+        local: LocalRequestData = Depends(LocalRequestData)
+):
+    """
+    Set the participants of an open communism
+
+    A 400 error will be returned if the communism is already closed or if
+    any participant is an external user without voucher or a disabled user.
+    A 404 error will be returned if the communism ID or the
+    user ID of any mentioned participant is unknown.
+    A 409 error will be returned if the community user is part of the participants.
+    """
+
+    model = await helpers.return_one(communism_id, models.Communism, local.session)
+
+    if not model.active:
+        raise BadRequest("Updating an already closed communism is not possible.", detail=str(model))
+
+    participants = _compress_participants(participants)
+    await _check_participants(participants, local)
+
+    remaining = list(participants)[:]
+    for c_u in model.participants:
+        for p in participants:
+            if c_u.user_id == p.user_id:
+                c_u.quantity = p.quantity
+                remaining.remove(p)
+                break
+        else:
+            local.session.delete(c_u)
+    for p in remaining:
+        local.session.add(models.CommunismUsers(communism_id=model.id, user_id=p.user_id, quantity=p.quantity))
+
     return await helpers.update_model(model, local, logger, helpers.ReturnType.SCHEMA)
