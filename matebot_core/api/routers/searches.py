def3e071043b3d64bb8e2f47e5db981e54ec1593
Chris <chris@hopfen.space>
2022-04-30 17:37:03 +0200

    Removed the unused 'GET /ballots' and 'GET /multitransactions' endpoints

--- matebot_core/api/routers/searches.py
+++ matebot_core/api/routers/searches.py
@@ -43,41 +43,6 @@ async def search_for_applications(
     )
 
 
-###########
-# BALLOTS #
-###########
-
-
-@router.get("/ballots", tags=["Searches"], response_model=List[schemas.Ballot])
-@versioning.versions(minimal=1)
-async def search_for_ballots(
-        id: Optional[pydantic.NonNegativeInt] = None,  # noqa
-        vote_id: Optional[pydantic.NonNegativeInt] = None,
-        ballot_for_poll: Optional[bool] = None,
-        ballot_for_refund: Optional[bool] = None,
-        local: LocalRequestData = Depends(LocalRequestData)
-):
-    """
-    Return all ballots that fulfill *all* constraints given as query parameters
-    """
-
-    def extended_filter(b: models.Ballot) -> bool:
-        if vote_id is not None and vote_id not in [v.id for v in b.votes]:
-            return False
-        if ballot_for_poll is not None and bool(b.polls) != ballot_for_poll:
-            return False
-        if ballot_for_refund is not None and bool(b.refunds) != ballot_for_refund:
-            return False
-        return True
-
-    return helpers.search_models(
-        models.Ballot,
-        local,
-        specialized_item_filter=extended_filter,
-        id=id
-    )
-
-
 ###############
 # CONSUMABLES #
 ###############
@@ -103,30 +68,6 @@ async def search_for_consumables(
         and (description is None or consumable.description == description)
         and (price is None or consumable.price == price)
     ]
-
-
-######################
-# MULTI TRANSACTIONS #
-######################
-
-
-@router.get("/multitransactions", tags=["Searches"], response_model=List[schemas.MultiTransaction])
-@versioning.versions(1)
-async def search_for_multi_transactions(
-        id: Optional[pydantic.NonNegativeInt] = None,  # noqa
-        base_amount: Optional[pydantic.NonNegativeInt] = None,
-        local: LocalRequestData = Depends(LocalRequestData)
-):
-    """
-    Return all multi transactions that fulfill *all* constraints given as query parameters
-    """
-
-    return helpers.search_models(
-        models.MultiTransaction,
-        local,
-        id=id,
-        base_amount=base_amount
-    )
 
 
 #########
