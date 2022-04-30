hopfenspace/MateBot

commit 21fd8063e522a8d60b9c18e509a26719ffbfc4e4

Moved the consumables from the database to the config file

matebot_core/schemas/config.py
@@ -2,9 +2,11 @@
 Special schemas for the configuration file and its properties
 """
 
-from typing import Dict
+from typing import Dict, List
 
 import pydantic
+
+from .bases import Consumable
 
 
 class GeneralConfig(pydantic.BaseModel):
@@ -78,5 +80,20 @@ class LoggingConfig(pydantic.BaseModel):
 class CoreConfig(pydantic.BaseModel):
     general: GeneralConfig
     server: ServerConfig
+    consumables: List[Consumable] = []
     database: DatabaseConfig
     logging: LoggingConfig
+
+    @pydantic.validator("consumables")
+    def enforce_consumable_constraints(
+            value: List[Consumable]  # noqa
+    ):
+        identifiers = {v.id for v in value}
+        if len(identifiers) != len(value):
+            raise ValueError("Field 'id' must be unique")
+        names = {v.name.lower() for v in value}
+        if len(names) != len(value):
+            raise ValueError("Field 'name' must be unique")
+        if identifiers != set(range(1, len(value) + 1)):
+            raise ValueError("Identifiers should be indexed with increasing natural numbers")
+        return value

matebot_core/api/routers/searches.py
@@ -93,17 +93,16 @@ async def search_for_consumables(
         local: LocalRequestData = Depends(LocalRequestData)
 ):
     """
-    Return all ballots that fulfill *all* constraints given as query parameters
+    Return all consumables that fulfill *all* constraints given as query parameters
     """
 
-    return helpers.search_models(
-        models.Consumable,
-        local,
-        id=id,
-        name=name,
-        description=description,
-        price=price
-    )
+    return [
+        consumable for consumable in local.config.consumables
+        if (id is None or consumable.id == id)
+        and (name is None or consumable.name == name)
+        and (description is None or consumable.description == description)
+        and (price is None or consumable.price == price)
+    ]
 
 
 ######################

matebot_core/persistence/models.py
@@ -172,31 +172,6 @@ class MultiTransaction(Base):
         )
 
 
-class Consumable(Base):
-    __tablename__ = "consumables"
-
-    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True, unique=True)
-    name = Column(String(255), unique=True)
-    description = Column(String(255), nullable=False, default="")
-    price = Column(Integer, nullable=False)
-
-    __table_args__ = (
-        CheckConstraint("price > 0"),
-    )
-
-    @property
-    def schema(self) -> schemas.Consumable:
-        return schemas.Consumable(
-            id=self.id,
-            name=self.name,
-            description=self.description,
-            price=self.price
-        )
-
-    def __repr__(self) -> str:
-        return f"Consumable(id={self.id}, name={self.name}, price={self.price})"
-
-
 class Refund(Base):
     __tablename__ = "refunds"
 

commit e8dd09918e7bff104ea2b97fb66cda070a69aa28

Improved the consumption, replaced the consumable IDs with the unique names
The bug was introduced in 21fd8063e522a8d60b9c18e509a26719ffbfc4e4

matebot_core/api/routers/transactions.py
@@ -9,7 +9,7 @@ import pydantic
 from fastapi import Depends
 
 from ._router import router
-from ..base import APIException, BadRequest, Conflict
+from ..base import APIException, BadRequest, Conflict, NotFound
 from ..dependency import LocalRequestData
 from .. import helpers, versioning
 from ...persistence import models
@@ -115,11 +115,10 @@ async def make_a_new_transaction(
         if user.external and user.voucher_id is None:
             raise BadRequest("You can't consume any goods, since you are an external user without voucher.")
 
-        consumable: models.Consumable = await helpers.return_one(
-            consumption.consumable_id,
-            models.Consumable,
-            local.session
-        )
+        consumables = [c for c in local.config.consumables if c.name == consumption.consumable]
+        if len(consumables) == 0:
+            raise NotFound(f"Consumable {consumption.consumable}")
+        consumable = consumables[0]
         reason = f"consume: {consumption.amount}x {consumable.name}"
         total = consumable.price * consumption.amount
         return create_transaction(user, community, total, reason, local.session, logger, local.tasks).schema

matebot_core/schemas/bases.py
@@ -95,7 +95,6 @@ class MultiTransaction(pydantic.BaseModel):
 
 
 class Consumable(pydantic.BaseModel):
-    id: pydantic.NonNegativeInt
     name: pydantic.constr(max_length=255)
     description: pydantic.constr(max_length=255)
     price: pydantic.PositiveInt
@@ -104,7 +103,7 @@ class Consumable(pydantic.BaseModel):
 class Consumption(pydantic.BaseModel):
     user: user_spec
     amount: pydantic.PositiveInt
-    consumable_id: pydantic.NonNegativeInt
+    consumable: pydantic.constr(max_length=255)
 
 
 class VoucherUpdateResponse(pydantic.BaseModel):

matebot_core/schemas/config.py
@@ -88,12 +88,6 @@ class CoreConfig(pydantic.BaseModel):
     def enforce_consumable_constraints(
             value: List[Consumable]  # noqa
     ):
-        identifiers = {v.id for v in value}
-        if len(identifiers) != len(value):
-            raise ValueError("Field 'id' must be unique")
-        names = {v.name.lower() for v in value}
-        if len(names) != len(value):
+        if len({v.name.lower() for v in value}) != len(value):
             raise ValueError("Field 'name' must be unique")
-        if identifiers != set(range(1, len(value) + 1)):
-            raise ValueError("Identifiers should be indexed with increasing natural numbers")
         return value