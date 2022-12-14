b09ed6d6bcd6b3e5ebcc160ce46d8a3e0e723fd0
Chris <chris@hopfen.space>
2022-12-14 17:21:43 +0100

    Updated the alembic env file to utilize the settings if available

--- matebot_core/persistence/alembic/env.py
+++ matebot_core/persistence/alembic/env.py
@@ -1,3 +1,4 @@
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,26 +6,27 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from matebot_core.settings import Settings
+from matebot_core.persistence.models import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-from matebot_core.persistence.models import Base
-target_metadata = Base.metadata
-
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-from matebot_core.settings import Settings
 settings = Settings()
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if "alembic" in sys.executable.lower():
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+target_metadata = Base.metadata
 
 
 def run_migrations_offline():
